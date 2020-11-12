fb7312cada514ea9a22f2cc5fa0be207179704a3
lundregan <lundregan.ethan@gmail.com>
2020-11-12 16:24:36 +0000

    wrote content for games

--- content/games/devanicastle.md
+++ content/games/devanicastle.md
@@ -4,3 +4,37 @@ img: /devanicastle.jpg
 description: Be careful when you enter Devani's castle. She hunts in the night, your only friend being the light!
 released: 2017
 ---
+
+# Devani's Castle
+
+Be careful when you enter Devani's castle. She hunts in the night, your only friend being the light!
+
+Devani's castle is full of hidden monsters and frightning illusions, do you dare to step in?
+
+If you do so, word of caution.
+
+## Features
+
+#### Intelligent Monsters
+
+Devani was a hell of a mad-scientist.
+
+The monsters will have you on the edge of your seat!
+
+#### Machine Learning
+Powering the monsters ability to analyise your emotional state as you play the game and make choices based on that information.
+
+#### Hyper-Realistic Teaxturing
+Allowing us to create the most realistic and frightning monsters ever to be seen in gaming!
+
+#### 
+
+## Honorables
+
+What some of our playtesters though of our demo.
+
+#### Martin Goodman
+> "May as well call this game 'Heart-attack simulator'"
+
+#### Sarah lockheart
+> "I do not dare step into Devanis Castle every again! Good thing this place isn't real... right?"
--- pages/games/_slug.vue
+++ pages/games/_slug.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-      <p>{{game.name}}</p>
+  <div class="flex justify-center text-white">
+      <div class="w-1/2 mt-8">
+        <img :src="game.img" alt="" class="m-4">
+        <nuxt-content :document="game" class="" />
+      </div>
   </div>
 </template>
 
@@ -14,6 +17,70 @@ export default {
   }
 </script>
 
-<style>
+<style lang="sass">
+@import url(http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,300italic,700)
+@import url(http://fonts.googleapis.com/css?family=Arimo:700,700italic)
+
+.nuxt-content
+  line-height: 1.85
+  font-family: roboto, sans-serif
+  font-weight: 300
+  margin: 0 auto
+  max-width: 48rem
+  line-height: 1.45
+  padding: .25rem
+  
+  p
+    font-size: 1rem
+    margin-bottom: 1.3rem
+  
+  a
+    color: #e53e3e
+
+  a:hover, a:focus, a:active
+    color: #2980b9
+
+  h1, h2, h3, h4
+    margin: 2.5rem 0 .5rem
+    font-weight: inherit
+    line-height: 1.42
+    font-family: Arimo, Helvetica, sans-serif
+
+  h1, h2, h3
+    margin-bottom: 1.15rem
+    padding-bottom: .5rem
+    text-align: center
+  
+  h1
+    margin-top: 0
+    border-bottom: 2px solid #fafafa
+    font-size: 3.998rem
+  
+  h2
+    font-size: 2.827rem
+  
+  h3  
+    font-size: 1.999rem
+  
+  h4
+    font-size: 1.414rem
+  
+  h5
+    font-size: 1.121rem
+  
+  h6
+    font-size: .88rem
+  
+  small
+    font-size: .707em
+  
+  img, canvas, iframe, video, svg, select, textarea
+    max-width: 100%
+
+  blockquote
+    padding: 1rem
+  
+  pre, code
+    background-color: #fafafa
 
 </style>
