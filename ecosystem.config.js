2b8181515237d9e156941223089b2541a5e72057
lundregan <lundregan.ethan@gmail.com>
2020-11-13 18:03:50 +0000

    set cluster instances to 2 for pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
       {
         name: 'desmoscorp',
         exec_mode: 'cluster',
-        instances: 'max', // Or a number of instances
+        instances: 2, // Or a number of instances
         script: './node_modules/nuxt/bin/nuxt.js',
         args: 'start'
       }
