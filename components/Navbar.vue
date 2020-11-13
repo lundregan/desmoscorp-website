537a009e4374917c623f98f7618e9323229353b4
lundregan <lundregan.ethan@gmail.com>
2020-11-13 10:49:12 +0000

    nicer navbar

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,20 +1,86 @@
 <template>
-  <div class="navbar shadow-md w-full p-2 flex justify-between align-center bg-blue-500">
+  <div class="navbar shadow-md w-full flex justify-between align-center text-white px-4 pb-2 pt-4">
     <div class="brand">
-      <p>DESMOS CORP</p>
+      <p class="font-semibold text-2xl text-red-600">DESMOS CORP</p>
+      <p class="text-xs pl-5">No Expense Innovation</p>
     </div>
-    <div class="corp-links w-1/5 flex justify-around">
-    <nuxt-link to="/">Home</nuxt-link>
-      <nuxt-link to="/stances">Stances</nuxt-link>
-      <nuxt-link to="/games">Games</nuxt-link>
-      <nuxt-link to="/news">News</nuxt-link>
+    <div class="corp-links w-1/2 lg:w-1/5 flex flex-col sm:flex-row justify-around text-right">
+      <div
+        v-for="link in links"
+        :key="link.id"
+        class=""
+      >
+        <nuxt-link 
+          :to="link.link"
+          class="text-xl"
+        >
+          <p class="grows">
+            {{link.name}}
+          </p>
+        </nuxt-link>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      links: [
+        {
+          link: '/',
+          name: 'Home'
+        },
+        {
+          link: '/stances',
+          name: 'Stances'
+        },
+        {
+          link: '/games',
+          name: 'Games'
+        },
+        {
+          link: '/news',
+          name: 'News'
+        }
+      ]
+    }
+  }
+}
 </script>
 
-<style>
+<style lang="sass">
+.navbar
+  background: #212121
+
+.grows
+  animation: grow-back 0.2s forwards linear, link-deselected 2s forwards
+
+.grows:hover
+  animation: grow 0.1s forwards ease-in-out, link-selected 1s forwards
+
+@keyframes grow-back
+  from
+    transform: scale(1.25)
+  to
+    transform: scale(1)
+
+@keyframes grow
+  from
+    transform: scale(1)
+  to
+    transform: scale(1.25)
+
+@keyframes link-selected
+  from
+    color: white
+  to
+    color: #c53030
+
+@keyframes link-deselected
+  from
+    color: #c53030
+  to
+    color: white
 </style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -24,7 +24,7 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
-  background: #212121;
+  background: #313131;
 }
 
 *,
